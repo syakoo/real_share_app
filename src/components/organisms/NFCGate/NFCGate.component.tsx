364bf984023e84ede3e7682407996b316607c163
syakoo <nichijou4009@gmail.com>
2020-04-17 16:29:17 +0900

    update NFCGate

--- src/components/organisms/NFCGate/NFCGate.component.tsx
+++ src/components/organisms/NFCGate/NFCGate.component.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { initNDEF, startNFCReadListen } from '../../../store/nfc/nfc.actions'
+
+import styles from './NFCGate.module.scss'
 // ____________________________________________________________________________
 //
 export const NFCGate: React.FC = ({ children }) => {
@@ -14,5 +16,25 @@ export const NFCGate: React.FC = ({ children }) => {
       dispatch(startNFCReadListen())
     }
   }, [enabled, dispatch])
-  return <>{enabled ? children : <div>NFC Did not supported</div>}</>
+  return <>{enabled ? children : <NFCDenied />}</>
 }
+
+const NFCDenied: React.FC = () => (
+  <div className={styles.denied}>
+    <h1>NFC Did not supported</h1>
+    <div>
+      <h2>How to set WebNFC enabled</h2>
+      <p>
+        If you're using Google Chrome, go to{' '}
+        <a href="chrome://flags/#enable-webnfc">
+          chrome://flags/#enable-webnfc
+        </a>{' '}
+        and enable it.
+      </p>
+      <p>
+        If that still doesn't work, either restart this browser or check your
+        phone's NFC settings.
+      </p>
+    </div>
+  </div>
+)
