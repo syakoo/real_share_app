1cd0fcf83b383ceae920049f83c1bd1cd07f82a8
syakoo <nichijou4009@gmail.com>
2020-04-17 16:33:22 +0900

    change .gitigore to push build directory

---
+++ build/precache-manifest.ec2a3d92a06ae585074b9bfc48258cce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d89ca7c7f6b2deb54f08486390e0cf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "744260999c229088fb91",
+    "url": "/static/css/main.91a7fc43.chunk.css"
+  },
+  {
+    "revision": "cdd475e6188f7cf08b5b",
+    "url": "/static/js/2.72c307cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72c307cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "744260999c229088fb91",
+    "url": "/static/js/main.37676d6f.chunk.js"
+  },
+  {
+    "revision": "6e2b8b9044ba7fd5daa8",
+    "url": "/static/js/runtime-main.6e707ae9.js"
+  },
+  {
+    "revision": "ac821891d6bf83612ed0908e59065c79",
+    "url": "/static/media/Nfc-icon-vector-01.ac821891.svg"
+  },
+  {
+    "revision": "794daaafc190d839a958b5a69af57a9c",
+    "url": "/static/media/home.794daaaf.svg"
+  },
+  {
+    "revision": "82b50d8d9acd7cb1e5165e4219a2e5c9",
+    "url": "/static/media/logo.82b50d8d.svg"
+  },
+  {
+    "revision": "3126da9b0dc717b087197060eadc287b",
+    "url": "/static/media/reconstruction.3126da9b.svg"
+  },
+  {
+    "revision": "88b1bd60c03bd7055512901c6b6cacb0",
+    "url": "/static/media/sharing.88b1bd60.svg"
+  }
+]);
