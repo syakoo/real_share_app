7d5601d54b33cb6cd258213e6f538e94f3dd3b3a
syakoo <nichijou4009@gmail.com>
2020-04-17 16:48:39 +0900

    Updates

---
+++ precache-manifest.556ffd72acd85a5892d60101f17631c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63238e93972fd8244b0819baa2a367dd",
+    "url": "/real_share_app/index.html"
+  },
+  {
+    "revision": "dc9b846cbc1a84076590",
+    "url": "/real_share_app/static/css/main.e0455335.chunk.css"
+  },
+  {
+    "revision": "441f567fccf191f240dc",
+    "url": "/real_share_app/static/js/2.15c1982f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/real_share_app/static/js/2.15c1982f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9b846cbc1a84076590",
+    "url": "/real_share_app/static/js/main.2347133f.chunk.js"
+  },
+  {
+    "revision": "6824e476eaafca7759f9",
+    "url": "/real_share_app/static/js/runtime-main.46533265.js"
+  },
+  {
+    "revision": "ac821891d6bf83612ed0908e59065c79",
+    "url": "/real_share_app/static/media/Nfc-icon-vector-01.ac821891.svg"
+  },
+  {
+    "revision": "794daaafc190d839a958b5a69af57a9c",
+    "url": "/real_share_app/static/media/home.794daaaf.svg"
+  },
+  {
+    "revision": "82b50d8d9acd7cb1e5165e4219a2e5c9",
+    "url": "/real_share_app/static/media/logo.82b50d8d.svg"
+  },
+  {
+    "revision": "3126da9b0dc717b087197060eadc287b",
+    "url": "/real_share_app/static/media/reconstruction.3126da9b.svg"
+  },
+  {
+    "revision": "88b1bd60c03bd7055512901c6b6cacb0",
+    "url": "/real_share_app/static/media/sharing.88b1bd60.svg"
+  }
+]);
